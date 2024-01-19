b5f096c1d530ca93f9d65e7802dd3446d3161c59
Leo-Bardo <julioalb4488@gmail.com>
2024-01-19 17:27:16 -0600

    script incremento

--- xplJS/index.php
+++ xplJS/index.php
@@ -4,8 +4,19 @@ $con = conectar();
 
 // 18 ENERO 23 CONFIGURAR PARA RECIBIR HOMOGENIZDORES DE LA BASE DE DATOS
 // CONFIGURAR BBDD PARA SEPARAR EQUIPO DE HOMOGENIZADORES, TANQUES Y DEMAS EQUIPO
-    $sql = "SELECT idCliente, cliente FROM clientes";
-    $resultadoCliente = $con->query($sql);
+// EL VALOR 2 DE LA TABLA EQUIPO INDICA LA SELECCIÓN DE HOMOGENIZADORES EXISTENTES, SE PREVEE EL CAMBIO PARA IGUALAR A VALOR VARCHAR
+    $sqlHomogenizador = "SELECT idEquipo, equipo FROM equipos WHERE tipoEquipo = 2";
+    $resultadoHomogenizador = $con->query($sqlHomogenizador);
+
+        $query = "SELECT la.codigoLiberacion, p.producto AS producto FROM liberacion_area la INNER JOIN productos p ON la.producto = p.idProducto";
+        $row = $con -> query($query);
+        $row = $row->fetch_assoc();
+        $seleccionProducto = $row['producto'];
+
+        $fecha = date(" Y-m-d");
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,33 +29,44 @@ $con = conectar();
 </head>
 <body>
 
-<form id="miFormulario" action="procesar.php" method="POST">
+<form id="miFormulario" action="controlHomogenizacion.php" method="POST">
         <div class="container">
-            <h2>Selecciona el área a procesar:</h2>
-
-            <label for="cliente">CLIENTE:</label>
-            <select id="cliente" name="cliente" class="cliente" required>
-                <option value="">Selecciona un cliente</option>
+            <div class="con-producto">
+                <label for="producto"><b>PRODUCTO:</b></label>
                 <?php
-                if ($resultadoCliente->num_rows > 0) {
-                    while ($fila = $resultadoCliente->fetch_assoc()) {
-                        echo "<option value='" . $fila["idCliente"] . "'>" . $fila["cliente"] . "</option>";
-                    }
-                }
+                echo $seleccionProducto;
                 ?>
-            </select>
+                <label for="fecha"><b>FECHA:</b></label>
+                <?php 
+                echo $fecha;
+                ?>
+            </div>
+<br>
+            <div>
+                <br>
+                <label for="equipo"><b>HOMOGENIZADOR:</b></label>
+                <select id="equipo" name="equipo" class="equipo" required>
+                    <option value="">Selecciona un Homogenizador</option>
+                    <?php
+                    if ($resultadoHomogenizador->num_rows > 0) {
+                        while ($fila = $resultadoHomogenizador->fetch_assoc()) {
+                            echo "<option value='" . $fila["idEquipo"] . "'>" . $fila["equipo"] . "</option>";
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
             <br>
             <!-- FIN TEMPORAL DEL DIV -->
-        </div>
-
-    <div id="contador">Número de Lote: 1</div>
-    <label for="dato">Dato:</label>
-    <input type="text" id="dato" name="dato" required>
     <button type="button" id="enviarBtn">Enviar y Incrementar</button>
-</form>
-
-
+    <br>
+</form> 
 <script src="js/script.js"></script>
+
+    <div id="contador"><b>Número de Lote: </b></div>
+        <label for="dato">Dato:</label>
+        <input type="text" id="dato" name="dato" required>
+    </div>
 
 </body>
 </html>
