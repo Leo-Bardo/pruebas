8a95f3aebda6956c2ba420879bb19665c1fd9464
Leo-Bardo <julioalb4488@gmail.com>
2024-01-22 17:25:15 -0600

    Script de incremento

--- xplJS/controlHomogenizacion.php
+++ xplJS/controlHomogenizacion.php
@@ -5,9 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $dato = isset($_POST['dato']) ? $_POST['dato'] : '';
 
     // Procesar la información como sea necesario
-    // Puedes realizar operaciones con la base de datos, validaciones, etc.
+        // Puedes realizar operaciones con la base de datos, validaciones, etc.
 
-    // Puedes enviar una respuesta al cliente si es necesario
+        // Puedes enviar una respuesta al cliente si es necesario
     echo "Datos recibidos: Dato=$dato";
 } else {
     // Si el formulario no ha sido enviado de manera correcta, redireccionar o mostrar un mensaje de error
