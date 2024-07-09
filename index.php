f73684830f6964ee7dd268fc28edcb34e87e8b1a
Leo-Bardo <julioalb4488@gmail.com>
2024-07-09 12:09:24 -0600

    diseño para checklist reporte diario de preparación

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
 	<link rel="stylesheet" href="css/styles.css">
+	<!-- INTEGRACIÓN DE FAVICON REVISAR COMO FUNCIONA -->
+	<link rel="icon" href="ruta_al_favicon.ico" type="image/x-icon">
 </head>
 <body>
 	<div class="title">
@@ -30,6 +32,29 @@
 				</div>
 				<div class="leftDiv" align="center">
 					<a href="tablaPestanas/index.php" title=""><h1>6 - Tabla con Pestañas</h1></a>
+				</div>
+			</div>
+			<div class="rightContainer">
+				<div class="rightDiv" align="center">
+					<a href="ejerciciosLogica/ejerciciosLogica.md" title=""><h1>6 - Ejercicios de Lógica</h1></a>
+				</div>
+				<div class="rightDiv" align="center">
+					<a href="dataList/index.php" title=""><h1>7 - Agregar Datos desde DataList</h1></a>
+				</div>
+				<div class="rightDiv" align="center">
+					<a href="asincronia/index.php" title=""><h1>8 - Asincronia con JS</h1></a>
+				</div>
+				<div class="rightDiv" align="center">
+					<a href="json/index.html" title=""><h1>9 - JSON JavaScript/PHP</h1></a>
+				</div>
+				<div class="rightDiv" align="center">
+					<a href="concentrado/index.php" title=""><h1>10 - Concentrado de Formulas</h1></a>
+				</div>
+				<div class="rightDiv" align="center">
+					<a href="generaJson/index.html" title=""><h1>11 - Arreglo procesado con PHP</h1></a>
+				</div>
+				<div class="rightDiv" align="center">
+					<a href="filtroBuscar/index.php" title=""><h1>12 - Filtro de Buscar</h1></a>
 				</div>
 			</div>
 			<div class="rightContainer">
