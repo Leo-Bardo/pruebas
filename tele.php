209791fa2d1a4f6494f5c0352ce459014d344428
Leo-Bardo <julioalb4488@gmail.com>
2024-08-23 09:47:13 -0600

    nuevos comentarios de tareas pendientes para softguida y sanitizantes

---
+++ tele.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>TELEVISIÓN</title>
+</head>
+<body>
+		<style>
+			html{
+  				background: linear-gradient(blue, pink);
+  				background-repeat: no-repeat;
+				color: 	white;
+				text-align: center;
+				height: 100%;
+			}
+
+		</style>
+	<h1>ESTE VIDEO ES TRAIDO DE YOUTUBE</h1>
+<iframe width="600" height="400" src="https://www.youtube.com/embed/fh9XbjlYZRE" title="La Gusana Ciega - Hey" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</body>
+</html>
